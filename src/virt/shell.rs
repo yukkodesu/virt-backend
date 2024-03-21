5348e58283c8f3398ea404524ebcd31a670d9d47
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-03-21 17:13:07 +0800

    feat: impl create_virt

--- src/virt/shell.rs
+++ src/virt/shell.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{fs::File, process::Command};
 
 use serde::{Deserialize, Serialize};
 
@@ -46,6 +46,80 @@ pub fn delete_snapshot(configure: SnapShotConfig) -> Result<String, std::io::Err
         .arg(configure.dom_name)
         .arg("--snapshotname")
         .arg(configure.snapshot_name);
+    let status = cmd.status()?;
+    match status.code() {
+        Some(code) => {
+            if code == 0 {
+                Ok("".to_string())
+            } else {
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    String::from_utf8(cmd.output()?.stderr).unwrap().trim(),
+                ))
+            }
+        }
+        None => Ok("".to_string()),
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+enum SystemType {
+    Linux,
+    Windows,
+}
+#[derive(Deserialize, Serialize)]
+pub struct CreateVirtConfig {
+    virt_name: String,
+    memory: String,
+    vcpu: String,
+    system: SystemType,
+    iso_filename: String,
+    disk_size: String,
+}
+
+pub fn create_virt(configure: CreateVirtConfig) -> Result<String, std::io::Error> {
+    let mut create_qcow2_cmd = Command::new("qemu-img");
+    create_qcow2_cmd
+        .arg("create")
+        .arg("-f")
+        .arg("qcow2")
+        .arg("disk.qcow2")
+        .arg(configure.disk_size)
+        .current_dir("/data_disk/create_test");
+    let status = create_qcow2_cmd.status()?;
+    match status.code() {
+        Some(code) => {
+            if code == 0 {
+                ()
+            } else {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    String::from_utf8(create_qcow2_cmd.output()?.stderr)
+                        .unwrap()
+                        .trim(),
+                ));
+            }
+        }
+        None => (),
+    };
+    let mut cmd = Command::new("virt-install");
+    cmd.arg("--name")
+        .arg(configure.virt_name)
+        .arg("--memory")
+        .arg(configure.memory)
+        .arg("--vcpu")
+        .arg(configure.vcpu)
+        .arg("--disk")
+        .arg("/data_disk/create_test/disk.qcow2")
+        .arg("--cdrom")
+        .arg(format!("/data_disk/create_test/cdrom.iso"))
+        .arg("--graphics")
+        .arg(format!(
+            "vnc,port={},password={},listen=0.0.0.0",
+            "5903", "abc123"
+        ))
+        .arg("--network")
+        .arg("default");
     let status = cmd.status()?;
     match status.code() {
         Some(code) => {
