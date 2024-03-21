64850f17a088d1112983adc906b99c8eb9dfc901
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-03-21 17:12:43 +0800

    feat: db add domains table

--- src/db/entity/domains.rs
+++ src/db/entity/domains.rs
@@ -3,14 +3,12 @@
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
-#[sea_orm(table_name = "user")]
+#[sea_orm(table_name = "domains")]
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
-    #[sea_orm(unique)]
-    pub username: String,
-    pub password: String,
-    pub permission: Option<String>,
+    pub name: String,
+    pub vnc_port: String,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
