8524ec294ef4c6745889dfe197dee29b681d60ec
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-03-29 09:51:49 +0800

    feat: impl edit-snapshot

--- src/virt/conn.rs
+++ src/virt/conn.rs
@@ -1,5 +1,8 @@
+use quick_xml::events::{BytesEnd, BytesStart, BytesText, Event};
+use quick_xml::{Reader, Writer};
 use roxmltree::Document;
-use std::{collections::HashMap, sync::mpsc::Sender, vec};
+use std::io::Cursor;
+use std::{collections::HashMap, sync::mpsc::Sender};
 use virt::{connect::Connect, domain::Domain, domain_snapshot::DomainSnapshot};
 
 use super::VirtError::{self, *};
@@ -84,8 +87,10 @@ pub fn list_snapshot(conn: &Connect, main_tx: &Sender<VirtResult>, params: &Vec<
             }
         });
     match res {
-        Ok(_) => main_tx.send(VirtResult::Ok(serde_json::to_string(&t).unwrap())),
-        Err(e) => main_tx.send(VirtResult::Err(e)),
+        Ok(_) => main_tx
+            .send(VirtResult::Ok(serde_json::to_string(&t).unwrap()))
+            .unwrap(),
+        Err(e) => main_tx.send(VirtResult::Err(e)).unwrap(),
     };
 }
 
@@ -116,6 +121,62 @@ pub fn list_snapshot_tree(conn: &Connect, main_tx: &Sender<VirtResult>, params: 
     }
 }
 
+fn edit_xml_text(
+    input: &String,
+    target_element: &str,
+    new_text: &str,
+    defined_depth: i64,
+) -> String {
+    let mut reader = Reader::from_str(&input);
+    reader.trim_text(true);
+    let mut writer = Writer::new_with_indent(Cursor::new(Vec::new()), b' ', 2);
+    let mut in_target_element = false;
+    let mut is_exist = false;
+    let mut depth = 0 as i64;
+    loop {
+        match reader.read_event() {
+            Ok(Event::Start(e)) => {
+                depth += 1;
+                if e.name().as_ref() == target_element.as_bytes() {
+                    in_target_element = true;
+                    is_exist = true;
+                }
+                writer.write_event(Event::Start(e.to_owned())).unwrap();
+            }
+            Ok(Event::End(e)) => {
+                if depth == defined_depth && !is_exist {
+                    let elem_start = BytesStart::new(target_element);
+                    writer.write_event(Event::Start(elem_start)).unwrap();
+                    let text = BytesText::new(new_text);
+                    writer.write_event(Event::Text(text)).unwrap();
+                    let elem_end = BytesEnd::new(target_element);
+                    writer.write_event(Event::End(elem_end)).unwrap();
+                }
+                depth -= 1;
+                if e.name().as_ref() == target_element.as_bytes() {
+                    in_target_element = false;
+                }
+                writer.write_event(Event::End(e.to_owned())).unwrap();
+            }
+            Ok(Event::Text(e)) => {
+                if in_target_element && depth == defined_depth + 1 {
+                    writer
+                        .write_event(Event::Text(BytesText::new(new_text)))
+                        .unwrap();
+                } else {
+                    writer.write_event(Event::Text(e.to_owned())).unwrap();
+                }
+            }
+            Ok(Event::Eof) => break,
+            Ok(e) => writer.write_event(e).unwrap(),
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+        }
+    }
+
+    let result = writer.into_inner().into_inner();
+    String::from_utf8(result).unwrap()
+}
+
 pub fn edit_snapshot(conn: &Connect, main_tx: &Sender<VirtResult>, params: &Vec<String>) {
     match serde_json::from_str::<SnapShotEditConfig>(&params[0]) {
         Ok(config) => {
@@ -123,7 +184,14 @@ pub fn edit_snapshot(conn: &Connect, main_tx: &Sender<VirtResult>, params: &Vec<
             match Domain::lookup_by_name(conn, &config.dom_name) {
                 Ok(dom) => match DomainSnapshot::lookup_by_name(&dom, &config.snapshot_name, 0) {
                     Ok(snapshot) => {
-                        
+                        let xml_str = snapshot.get_xml_desc(0).unwrap();
+                        let mut new_xml =
+                            edit_xml_text(&xml_str, "name", &config.new_snapshot_name, 1);
+                        if let Some(description) = config.description {
+                            new_xml =
+                                edit_xml_text(&new_xml, "description", &description, 1);
+                        }
+                        // println!("{}", new_xml);
                         main_tx
                             .send(VirtResult::Ok("Successfully edit snapshot".to_string()))
                             .unwrap();
