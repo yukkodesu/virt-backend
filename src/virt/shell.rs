9ffd817e820e10953b4228dea209eb3bf4075e60
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-04-13 11:36:30 +0800

    feat: create snapshot from snapshot

--- src/virt/shell.rs
+++ src/virt/shell.rs
@@ -6,11 +6,31 @@ use std::{
 use super::{AltDomStateCommand, CreateVirtConfig, SnapShotConfig};
 
 pub fn create_snapshot(configure: SnapShotConfig) -> Result<String, std::io::Error> {
+    let now = SystemTime::now();
+    let unix_timestamp = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
+    let temp_snapshot_str = "temp_snapshot".to_string() + &unix_timestamp.to_string();
+    if let Some(parent) = &configure.parent {
+        // create tmp snapshot to store latest change
+        let mut cmd = Command::new("virsh");
+        cmd.arg("snapshot-create-as")
+            .arg(&configure.dom_name)
+            .arg("--name")
+            .arg(&temp_snapshot_str);
+        let _ = cmd.status()?;
+
+        // revert to parent
+        let mut cmd = Command::new("virsh");
+        cmd.arg("snapshot-revert")
+            .arg(&configure.dom_name)
+            .arg(&parent);
+        let _ = cmd.status()?;
+
+    }
     let mut cmd = Command::new("virsh");
     cmd.arg("snapshot-create-as")
-        .arg(configure.dom_name)
+        .arg(&configure.dom_name)
         .arg("--name")
-        .arg(configure.snapshot_name);
+        .arg(&configure.snapshot_name);
     if let Some(des) = configure.description {
         cmd.arg("--description").arg(des);
     }
@@ -23,16 +43,33 @@ pub fn create_snapshot(configure: SnapShotConfig) -> Result<String, std::io::Err
     match status.code() {
         Some(code) => {
             if code == 0 {
-                Ok("".to_string())
+                ()
             } else {
-                Err(std::io::Error::new(
+                return Err(std::io::Error::new(
                     std::io::ErrorKind::InvalidInput,
                     String::from_utf8(cmd.output()?.stderr).unwrap().trim(),
                 ))
             }
         }
-        None => Ok("".to_string()),
-    }
+        None => return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "No status code".to_string(),
+        )),
+    };
+    //create complete, revert to latest tmp snapshot
+    let mut cmd = Command::new("virsh");
+    cmd.arg("snapshot-revert")
+        .arg(&configure.dom_name)
+        .arg(&temp_snapshot_str);
+    let _ = cmd.status()?;
+
+    // delete tmp snapshot
+    let mut cmd = Command::new("virsh");
+    cmd.arg("snapshot-delete")
+        .arg(&configure.dom_name)
+        .arg(&temp_snapshot_str);
+    let _ = cmd.status()?;
+    Ok("Success".to_string())
 }
 
 pub fn delete_snapshot(configure: SnapShotConfig) -> Result<String, std::io::Error> {
@@ -75,7 +112,10 @@ pub fn set_current_snapshot(configure: SnapShotConfig) -> Result<String, std::io
                 ))
             }
         }
-        None => Ok("".to_string()),
+        None => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "No status code".to_string(),
+        )),
     }
 }
 
@@ -136,7 +176,10 @@ pub fn create_virt(configure: CreateVirtConfig) -> Result<String, std::io::Error
                 ))
             }
         }
-        None => Ok("".to_string()),
+        None =>  Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "No status code".to_string(),
+        )),
     }
 }
 
